555bfdda9a28ab47c3b2cfae017d3751b823042d
Shuy935 <jessus_2500@hotmail.com>
2023-04-28 09:40:48 -0600

    Estructura lista 280423

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', function(req, res){
     res.send('Hola mundo! ');
 });
 
+app.get('/saludo', function(req, res){
+    res.send('Hola desde la API ');
+});
+
+app.get('/despedida', function(req, res){
+    res.send('Adios desde la API ');
+});
+
 app.listen(3000, function(){
     console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
 });
