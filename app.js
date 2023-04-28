fc6f7e4a4e9796988ac8fed10dc3a4329d172ada
Shuy935 <jessus_2500@hotmail.com>
2023-04-28 09:52:28 -0600

    Servidor con bodyparser

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var express = require('express');
-var app = express();
+const express = require('express');
 
-app.get('/', function(req, res){
-    res.send('Hola mundo! ');
-});
+const port = 3002;
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const app = express();
 
-app.get('/saludo', function(req, res){
-    res.send('Hola desde la API ');
-});
+//Usar node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
 
-app.get('/despedida', function(req, res){
-    res.send('Adios desde la API ');
-});
+routes(app);
 
-app.listen(3000, function(){
-    console.log('Aplicacion ejemplo, escuchando el puerto 3000!');
+//iniciar el servidor
+const server = app.listen(port, (error) => {
+    if(error) return console.log('Error: ${error}');
+    
+    console.log('El servidor escucha en el puerto ${server.address().port}');
 });
